fe5fa71677de04969f4379753b47999ff7acea29
obadoni2 <obadoniemma@gmail.com>
2024-03-13 11:01:02 -0700

    dtrj

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]
+word_last__2 = word[-2:]
+middle_word =word[1:-1]
+print(f"Firstn 3 letters:{word_first_:s}")
+print(f"last 2 letters:{word_last_2:s}")
+print(f"Middle word: {middle_word:s}")
