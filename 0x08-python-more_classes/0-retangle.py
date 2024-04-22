8cf53f07164837387114117b755fb8919d41594f
obadoni2 <obadoniemma@gmail.com>
2024-04-22 11:31:08 +0100

    code in python

---
+++ 0x08-python-more_classes/0-retangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+"""Defines an empty Rectangle class using the pass keyword"""
+
+
+class Rectangle:
+        """Represent a rectangle."""
+            pass#!/usr/bin/python3
+        # 0-rectangle.py
+        """Defines an empty Rectangle class using the pass keyword"""
+
+
+        class Rectangle:
+                """Represent a rectangle."""
+                    pass
