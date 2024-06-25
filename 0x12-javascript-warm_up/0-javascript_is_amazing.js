2e34c384d7ab969683e6a506be13cc0fd7b76c79
obadoni2 <obadoniemma@gmail.com>
2024-06-25 19:19:02 +0100

    jhk

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-<<<<<<< HEAD
-console.log(myVar);
-=======
-console.log(myVar);
->>>>>>> fee3492ab8b8675fb611328b0db36e36e4322a2d
+// Shebang line for Unix-like systems
+
+const myVar = 'JavaScript is amazing'; // Declare a constant variable
+console.log(myVar); // Log the value of myVar to the console
